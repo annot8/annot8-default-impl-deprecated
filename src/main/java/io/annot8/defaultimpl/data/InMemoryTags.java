dcb788f81987c22adab5d4aacb9fa710fe7a7478
James Baker <james.d.baker@gmail.com>
2018-02-03 16:51:04 +0000

    Implementation for Tags

---
+++ src/main/java/io/annot8/defaultimpl/data/InMemoryTags.java
@@ -0,0 +1,58 @@
+package io.annot8.defaultimpl.data;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+import io.annot8.core.data.Tags;
+import io.annot8.core.exceptions.IncompleteException;
+
+/**
+ * Provides an in-memory implementation of the Tags interface,
+ * backed by a HashSet.
+ */
+public class InMemoryTags implements Tags {
+
+  private Set<String> tags;
+  
+  /**
+   * Private constructor, used by the Builder
+   */
+  private InMemoryTags(Set<String> tags) {
+    this.tags = tags;
+  }
+  
+  @Override
+  public Stream<String> get() {
+    return tags.stream();
+  }
+  
+  public static class Builder implements Tags.Builder{
+    private Set<String> tags = new HashSet<>();
+    
+    @Override
+    public io.annot8.core.data.Tags.Builder from(Tags from) {
+      from.get().forEach(this::addTag);
+      return this;
+    }
+
+    @Override
+    public Tags build() throws IncompleteException {
+      return new InMemoryTags(tags);
+    }
+
+    @Override
+    public io.annot8.core.data.Tags.Builder addTag(String tag) {
+      this.tags.add(tag);
+      return this;
+    }
+
+    @Override
+    public io.annot8.core.data.Tags.Builder addTags(Collection<String> tags) {
+      this.tags.addAll(tags);
+      return this;
+    }
+    
+  }
+
+}
---
+++ src/test/java/io/annot8/defaultimpl/data/InMemoryTagsTest.java
@@ -0,0 +1,39 @@
+package io.annot8.defaultimpl.data;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+import io.annot8.core.data.Tags;
+import io.annot8.core.exceptions.IncompleteException;
+
+public class InMemoryTagsTest {
+  
+  @Test
+  public void testTags() throws IncompleteException {
+    Tags tags1 = new InMemoryTags.Builder()
+        .addTag("TAG1")
+        .addTags(Arrays.asList("TAG2", "TAG3"))
+        .build();
+
+    List<String> tagsList1 = tags1.get().collect(Collectors.toList());
+    assertEquals(3, tagsList1.size());
+    assertTrue(tagsList1.contains("TAG1"));
+    assertTrue(tagsList1.contains("TAG2"));
+    assertTrue(tagsList1.contains("TAG3"));
+
+    Tags tags2 = new InMemoryTags.Builder()
+        .from(tags1)
+        .addTag("TAG4")
+        .build();
+
+    List<String> tagsList2 = tags2.get().collect(Collectors.toList());
+    assertEquals(4, tagsList2.size());
+    assertTrue(tagsList2.contains("TAG1"));
+    assertTrue(tagsList2.contains("TAG2"));
+    assertTrue(tagsList2.contains("TAG3"));
+    assertTrue(tagsList2.contains("TAG4"));
+  }
+}
