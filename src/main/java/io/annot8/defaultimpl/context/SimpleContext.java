c48dad1583ba1088e6bc6bfb4095df1fdfc2648e
James Baker <james.d.baker@gmail.com>
2018-02-05 09:22:32 +0000

    Implementation of Context

---
+++ src/main/java/io/annot8/defaultimpl/context/SimpleContext.java
@@ -0,0 +1,82 @@
+package io.annot8.defaultimpl.context;
+
+import io.annot8.core.components.Resource;
+import io.annot8.core.context.Context;
+import io.annot8.core.settings.Settings;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Simple implementation of Context, backed by a HashMap to store resources
+ */
+public class SimpleContext implements Context{
+
+  private Map<String, Resource> resources = new HashMap<>();
+  private Settings settings = null;
+
+  /**
+   * Create a new instance, with no settings and no resources
+   */
+  public SimpleContext(){
+    //Default constructor, with no settings
+  }
+
+  /**
+   * Create a new instance, with the specified settings and no resources
+   */
+  public SimpleContext(Settings settings){
+    this.settings = settings;
+  }
+
+  /**
+   * Create a new instance, with no settings and the specified resources
+   */
+  public SimpleContext(Map<String, Resource> resources){
+    this.resources.putAll(resources);
+  }
+
+  /**
+   * Create a new instance, with the specified settings and resources
+   */
+  public SimpleContext(Settings settings, Map<String, Resource> resources){
+    this.settings = settings;
+    this.resources.putAll(resources);
+  }
+
+  /**
+   * Add a new resource to the context object
+   */
+  public void addResource(String key, Resource resource){
+    resources.put(key, resource);
+  }
+
+  @Override
+  public Optional<Settings> getSettings() {
+    return Optional.ofNullable(settings);
+  }
+
+  @Override
+  public <T extends Resource> Optional<T> getResource(String s, Class<T> aClass) {
+    Resource r = resources.get(s);
+
+    if(r == null || !aClass.isAssignableFrom(r.getClass())) {
+      return Optional.empty();
+    }else{
+      return Optional.of(aClass.cast(r));
+    }
+  }
+
+  @Override
+  public Stream<String> getResourceKeys() {
+    return resources.keySet().stream();
+  }
+
+  @Override
+  public <T extends Resource> Stream<T> getResources(Class<T> aClass) {
+    return resources.values().stream()
+        .filter(r -> aClass.isAssignableFrom(r.getClass()))
+        .map(r -> aClass.cast(r));
+  }
+}
---
+++ src/test/java/io/annot8/defaultimpl/context/SimpleContextTest.java
@@ -0,0 +1,121 @@
+package io.annot8.defaultimpl.context;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import io.annot8.core.components.Capabilities;
+import io.annot8.core.components.Resource;
+import io.annot8.core.settings.Settings;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+
+public class SimpleContextTest {
+  @Test
+  public void testSimpleContextDefault(){
+    Resource r1 = mock(Resource.class);
+    Resource r2 = mock(TestResource.class);
+
+    SimpleContext context = new SimpleContext();
+    context.addResource("resource1", r1);
+    context.addResource("resource2", r2);
+
+    assertFalse(context.getSettings().isPresent());
+
+    assertFalse(context.getResource("foo", Resource.class).isPresent());
+    assertFalse(context.getResource("resource1", TestResource.class).isPresent());
+    assertTrue(context.getResource("resource1", Resource.class).isPresent());
+
+    List<String> keys = context.getResourceKeys().collect(Collectors.toList());
+    assertEquals(2, keys.size());
+    assertTrue(keys.contains("resource1"));
+    assertTrue(keys.contains("resource2"));
+
+    List<TestResource> resources = context.getResources(TestResource.class).collect(Collectors.toList());
+    assertEquals(1, resources.size());
+    assertEquals(r2, resources.get(0));
+  }
+
+  @Test
+  public void testSimpleContextMap(){
+    Resource r1 = mock(Resource.class);
+    Resource r2 = mock(TestResource.class);
+
+    Map<String, Resource> r = new HashMap<>();
+    r.put("resource1", r1);
+    r.put("resource2", r2);
+
+    SimpleContext context = new SimpleContext(r);
+
+    assertFalse(context.getSettings().isPresent());
+
+    assertFalse(context.getResource("foo", Resource.class).isPresent());
+    assertFalse(context.getResource("resource1", TestResource.class).isPresent());
+    assertTrue(context.getResource("resource1", Resource.class).isPresent());
+
+    List<String> keys = context.getResourceKeys().collect(Collectors.toList());
+    assertEquals(2, keys.size());
+    assertTrue(keys.contains("resource1"));
+    assertTrue(keys.contains("resource2"));
+
+    List<TestResource> resources = context.getResources(TestResource.class).collect(Collectors.toList());
+    assertEquals(1, resources.size());
+    assertEquals(r2, resources.get(0));
+  }
+
+  @Test
+  public void testSimpleContextSettings(){
+    Settings s = mock(Settings.class);
+
+    SimpleContext context = new SimpleContext(s);
+
+    assertTrue(context.getSettings().isPresent());
+    assertEquals(s, context.getSettings().get());
+
+    List<String> keys = context.getResourceKeys().collect(Collectors.toList());
+    assertTrue(keys.isEmpty());
+
+    List<Resource> resources = context.getResources(Resource.class).collect(Collectors.toList());
+    assertTrue(resources.isEmpty());
+  }
+
+  @Test
+  public void testSimpleContextSettingsAndMap(){
+    Resource r1 = mock(Resource.class);
+    Resource r2 = mock(TestResource.class);
+    Settings s = mock(Settings.class);
+
+    Map<String, Resource> r = new HashMap<>();
+    r.put("resource1", r1);
+    r.put("resource2", r2);
+
+    SimpleContext context = new SimpleContext(s, r);
+
+    assertTrue(context.getSettings().isPresent());
+    assertEquals(s, context.getSettings().get());
+
+    assertFalse(context.getResource("foo", Resource.class).isPresent());
+    assertFalse(context.getResource("resource1", TestResource.class).isPresent());
+    assertTrue(context.getResource("resource1", Resource.class).isPresent());
+
+    List<String> keys = context.getResourceKeys().collect(Collectors.toList());
+    assertEquals(2, keys.size());
+    assertTrue(keys.contains("resource1"));
+    assertTrue(keys.contains("resource2"));
+
+    List<TestResource> resources = context.getResources(TestResource.class).collect(Collectors.toList());
+    assertEquals(1, resources.size());
+    assertEquals(r2, resources.get(0));
+  }
+
+  private static class TestResource implements Resource{
+    @Override
+    public Capabilities getCapabilities(Settings settings) {
+      return mock(Capabilities.class);
+    }
+  }
+}
