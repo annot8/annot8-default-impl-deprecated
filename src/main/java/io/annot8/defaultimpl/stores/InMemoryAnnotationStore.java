dd0aa56a420c203327603732845ae8feecbb5b93
James Baker <james.d.baker@gmail.com>
2018-02-05 08:41:31 +0000

    Implementation of AnnotationStore

---
+++ src/main/java/io/annot8/defaultimpl/stores/InMemoryAnnotationStore.java
@@ -0,0 +1,70 @@
+package io.annot8.defaultimpl.stores;
+
+import io.annot8.core.annotations.Annotation;
+import io.annot8.core.exceptions.IncompleteException;
+import io.annot8.core.stores.AnnotationStore;
+import io.annot8.defaultimpl.annotations.SimpleAnnotation;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * In memory implementation, backed by a HashMap, of AnnotationStore
+ */
+public class InMemoryAnnotationStore implements AnnotationStore {
+
+  private Map<String, Annotation> annotations = new HashMap<>();
+  private final Class<? extends Annotation.Builder> annotationBuilderClass;
+
+  /**
+   * Construct a new instance of this class using SimpleAnnotation.Builder as the annotation
+   * builder
+   */
+  public InMemoryAnnotationStore() {
+    annotationBuilderClass = SimpleAnnotation.Builder.class;
+  }
+
+  /**
+   * Construct a new instance of this class using the specified Annotation.Builder as the annotation
+   * builder
+   */
+  public InMemoryAnnotationStore(Class<? extends Annotation.Builder> annotationBuilderClass) {
+    this.annotationBuilderClass = annotationBuilderClass;
+  }
+
+  @Override
+  public Annotation.Builder getBuilder() {
+    try {
+      return annotationBuilderClass.getConstructor().newInstance();
+    } catch (NoSuchMethodException | IllegalAccessException | InstantiationException | InvocationTargetException e) {
+      //TODO: Log error
+      return new SimpleAnnotation.Builder();
+    }
+  }
+
+  @Override
+  public Annotation save(Annotation.Builder builder) throws IncompleteException {
+    Annotation a = builder.build();
+
+    annotations.put(a.getId(), a);
+
+    return a;
+  }
+
+  @Override
+  public void delete(Annotation annotation) {
+    annotations.remove(annotation.getId(), annotation);
+  }
+
+  @Override
+  public Stream<Annotation> getAll() {
+    return annotations.values().stream();
+  }
+
+  @Override
+  public Optional<Annotation> getById(String s) {
+    return Optional.ofNullable(annotations.get(s));
+  }
+}
---
+++ src/test/java/io/annot8/defaultimpl/stores/InMemoryAnnotationStoreTest.java
@@ -0,0 +1,32 @@
+package io.annot8.defaultimpl.stores;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import io.annot8.core.annotations.Annotation;
+import io.annot8.core.exceptions.IncompleteException;
+import io.annot8.core.stores.AnnotationStore;
+import io.annot8.defaultimpl.bounds.NoBounds;
+import org.junit.jupiter.api.Test;
+
+public class InMemoryAnnotationStoreTest {
+  @Test
+  public void testInMemoryAnnotationStore() throws IncompleteException{
+    AnnotationStore store = new InMemoryAnnotationStore();
+
+    assertEquals(0, store.getAll().count());
+
+    Annotation a = store.save(store.getBuilder()
+        .withBounds(NoBounds.getInstance())
+        .withContent("TEST_CONTENT")
+        .withType("TEST"));
+
+    assertEquals(1, store.getAll().count());
+    store.getAll().forEach(annot -> assertEquals(a, annot));
+    assertEquals(a, store.getById(a.getId()).get());
+    assertFalse(store.getById("TEST").isPresent());
+
+    store.delete(a);
+    assertEquals(0, store.getAll().count());
+  }
+}
