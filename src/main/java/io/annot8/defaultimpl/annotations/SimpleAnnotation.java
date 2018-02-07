46ccea5e7774d3f8512bf2ae0a73862bf5ef6532
Chris Flatley <chris@committed.io>
2018-02-07 17:34:28 +0000

    Align annotation with core and make tests more granular

--- src/main/java/io/annot8/defaultimpl/annotations/SimpleAnnotation.java
+++ src/main/java/io/annot8/defaultimpl/annotations/SimpleAnnotation.java
@@ -1,36 +1,38 @@
 package io.annot8.defaultimpl.annotations;
 
-import java.util.Objects;
-import java.util.UUID;
+import io.annot8.common.properties.EmptyImmutableProperties;
+import io.annot8.common.stores.SaveFromBuilder;
 import io.annot8.core.annotations.Annotation;
 import io.annot8.core.bounds.Bounds;
 import io.annot8.core.exceptions.IncompleteException;
 import io.annot8.core.properties.ImmutableProperties;
 import io.annot8.core.properties.MutableProperties;
 import io.annot8.core.properties.Properties;
-import io.annot8.common.properties.EmptyImmutableProperties;
 import io.annot8.defaultimpl.properties.SimpleImmutableProperties;
 import io.annot8.defaultimpl.properties.SimpleMutableProperties;
+import java.util.Objects;
+import java.util.UUID;
 
 /**
  * Simple implementation of Annotation interface
  */
 public class SimpleAnnotation implements Annotation {
-  
+
   private final String id;
   private final String type;
   private final ImmutableProperties properties;
   private final Bounds bounds;
   private final String content;
-  
-  private SimpleAnnotation(final String id, final String type, final ImmutableProperties properties, final Bounds bounds, final String content) {
+
+  private SimpleAnnotation(final String id, final String type, final ImmutableProperties properties,
+      final Bounds bounds, final String content) {
     this.id = id;
     this.type = type;
     this.properties = properties;
     this.bounds = bounds;
     this.content = content;
   }
-  
+
   @Override
   public String getId() {
     return id;
@@ -55,43 +57,39 @@ public class SimpleAnnotation implements Annotation {
   public String getContentName() {
     return content;
   }
-  
+
   @Override
   public String toString() {
-    return this.getClass().getName() + " [id=" + id + ", type=" + type + ", contentName=" + content + ", bounds=" + bounds + ", properties="+properties +"]";
+    return this.getClass().getName() + " [id=" + id + ", type=" + type + ", contentName=" + content
+        + ", bounds=" + bounds + ", properties=" + properties + "]";
   }
-  
+
   @Override
   public int hashCode() {
     return Objects.hash(id, type, properties, bounds, content);
   }
-  
+
   @Override
   public boolean equals(Object o) {
-    if(!(o instanceof Annotation))
-      return false;
-    
-    Annotation a = (Annotation) o;
-    
-    return Objects.equals(id, a.getId()) && Objects.equals(type, a.getType())
-        && Objects.equals(properties, a.getProperties()) && Objects.equals(bounds, a.getBounds())
-        && Objects.equals(content, a.getContentName());
+    return equalsAnnotation(o);
   }
 
   /**
-   * Builder class for SimpleAnnotation, using UUID for generating new IDs
-   * and InMemoryImmutableProperties or EmptyImmutableProperties for the
-   * properties. 
+   * Builder class for SimpleAnnotation, using UUID for generating new IDs and
+   * InMemoryImmutableProperties or EmptyImmutableProperties for the properties.
    */
-  public static class Builder implements Annotation.Builder{
-    private String id = UUID.randomUUID().toString();
+  public static class Builder implements Annotation.Builder {
+
+    private final SaveFromBuilder<Annotation, Annotation> saver;
+    private final String content;
     private String type = null;
     private MutableProperties properties = new SimpleMutableProperties();
     private Bounds bounds = null;
-    private final String content;
+    private String id = null;
 
-    public Builder(String content) {
-        this.content = content;
+    public Builder(String content, SaveFromBuilder<Annotation, Annotation> saver) {
+      this.content = content;
+      this.saver = saver;
     }
 
     @Override
@@ -114,7 +112,7 @@ public class SimpleAnnotation implements Annotation {
 
     @Override
     public io.annot8.core.annotations.Annotation.Builder newId() {
-      this.id = UUID.randomUUID().toString();
+      this.id = null;
       return this;
     }
 
@@ -130,26 +128,31 @@ public class SimpleAnnotation implements Annotation {
 
     @Override
     public Annotation save() throws IncompleteException {
-      if(id == null)
-        throw new IncompleteException("ID has not been set");
-      
-      if(type == null)
+      if (id == null) {
+        id = UUID.randomUUID().toString();
+      }
+
+      if (type == null) {
         throw new IncompleteException("Type has not been set");
-      
-      if(bounds == null)
+      }
+
+      if (bounds == null) {
         throw new IncompleteException("Bounds has not been set");
-      
-      if(content == null)
+      }
+
+      if (content == null) {
         throw new IncompleteException("Content name has not been set");
-      
+      }
+
       ImmutableProperties immutableProperties;
-      if(properties.getAll().isEmpty()) {
+      if (properties.getAll().isEmpty()) {
         immutableProperties = EmptyImmutableProperties.getInstance();
-      }else {
+      } else {
         immutableProperties = new SimpleImmutableProperties.Builder().from(properties).save();
       }
-      
-      return new SimpleAnnotation(id, type, immutableProperties, bounds, content);
+
+      Annotation annotation = new SimpleAnnotation(id, type, immutableProperties, bounds, content);
+      return saver.save(annotation);
     }
 
     @Override
@@ -157,6 +160,6 @@ public class SimpleAnnotation implements Annotation {
       this.bounds = bounds;
       return this;
     }
-    
+
   }
 }
--- src/test/java/io/annot8/defaultimpl/annotations/SimpleAnnotationTest.java
+++ src/test/java/io/annot8/defaultimpl/annotations/SimpleAnnotationTest.java
@@ -1,64 +1,188 @@
 package io.annot8.defaultimpl.annotations;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
-import java.util.Map;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.clearInvocations;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import io.annot8.common.stores.SaveFromBuilder;
 import io.annot8.core.annotations.Annotation;
+import io.annot8.core.bounds.Bounds;
 import io.annot8.core.exceptions.IncompleteException;
-import io.annot8.common.bounds.NoBounds;
-import io.annot8.common.properties.EmptyImmutableProperties;
+import io.annot8.defaultimpl.annotations.SimpleAnnotation.Builder;
 import io.annot8.defaultimpl.properties.SimpleMutableProperties;
+import io.annot8.test.TestBounds;
+import io.annot8.test.TestConstants;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SimpleAnnotationTest {
+
+  private final Bounds bounds = new TestBounds();
+  private SaveFromBuilder<Annotation, Annotation> annotationSaver;
+
+  @BeforeEach
+  public void beforeEach() {
+    annotationSaver = mock(SaveFromBuilder.class);
+    when(annotationSaver.save(any(Annotation.class))).then(a -> a.getArguments()[0]);
+  }
+
   @Test
-  public void testSimpleAnnotation() throws IncompleteException{
-    //TODO: Can we use mocks to remove dependency on InMemoryMutableProperties and NoBounds?
-    
-    try {
-      new SimpleAnnotation.Builder().build();
-      fail("Expected exception not thrown");
-    }catch(IncompleteException ie) {
-      //Expected exception, do nothing
-    }
-    
-    try {
-      new SimpleAnnotation.Builder().withType("TEST").build();
-      fail("Expected exception not thrown");
-    }catch(IncompleteException ie) {
-      //Expected exception, do nothing
-    }
-    
-    try {
-      new SimpleAnnotation.Builder().withType("TEST").withBounds(NoBounds.getInstance()).build();
-      fail("Expected exception not thrown");
-    }catch(IncompleteException ie) {
-      //Expected exception, do nothing
-    }
-    
-    Annotation a1 = new SimpleAnnotation.Builder().withType("TEST").withBounds(NoBounds.getInstance()).withContent("TEST_CONTENT").build();
+  public void testIncompleteBuilderNothingSet() {
+    assertThrows(IncompleteException.class,
+        new Builder(TestConstants.CONTENT_NAME, annotationSaver)::save);
+  }
+
+  @Test
+  public void testIncompleteBuilderNoBounds() {
+    assertThrows(IncompleteException.class,
+        new Builder(TestConstants.CONTENT_NAME, annotationSaver).withType("TEST")::save);
+  }
+
+  @Test
+  public void testIncompleteBuilderNoType() {
+    assertThrows(IncompleteException.class,
+        new Builder(TestConstants.CONTENT_NAME, annotationSaver).withBounds(bounds)::save);
+  }
+
+  @Test
+  public void testMinimal() throws IncompleteException {
+    Annotation a1 = new SimpleAnnotation.Builder(TestConstants.CONTENT_NAME, annotationSaver)
+        .withType("TEST")
+        .withBounds(bounds).save();
     assertNotNull(a1.getId());
     assertEquals("TEST", a1.getType());
-    assertEquals(NoBounds.getInstance(), a1.getBounds());
-    assertEquals("TEST_CONTENT", a1.getContentName());
-    assertEquals(EmptyImmutableProperties.getInstance(), a1.getProperties());
-    
-    Annotation a2 = new SimpleAnnotation.Builder().from(a1).withProperty("key1", Integer.valueOf(17)).withProperty("key2", false).build();
+    assertEquals(bounds, a1.getBounds());
+    assertEquals(TestConstants.CONTENT_NAME, a1.getContentName());
+    assertTrue(a1.getProperties().getAll().isEmpty());
+
+    verify(annotationSaver, only()).save(a1);
+  }
+
+
+  @Test
+  public void testWithProperty() throws IncompleteException {
+
+    Annotation a2 = new SimpleAnnotation.Builder(TestConstants.CONTENT_NAME, annotationSaver)
+        .withType(TestConstants.ANNOTATION_TYPE)
+        .withBounds(bounds)
+        .withProperty("key1", 17)
+        .withProperty("key2", false)
+        .save();
     assertNotNull(a2.getId());
-    assertEquals("TEST", a2.getType());
-    assertEquals(NoBounds.getInstance(), a2.getBounds());
-    assertEquals("TEST_CONTENT", a2.getContentName());
-    
-    Map<String, Object> properties2 = a2.getProperties().getAll();
-    assertEquals(2, properties2.size());
-    assertEquals(Integer.valueOf(17), properties2.get("key1"));
-    assertEquals(false, properties2.get("key2"));
-    
-    Annotation a3 = new SimpleAnnotation.Builder().withType("TEST").withBounds(NoBounds.getInstance()).withProperties(new SimpleMutableProperties(properties2)).withContent("TEST_CONTENT").build();
+    assertEquals(TestConstants.ANNOTATION_TYPE, a2.getType());
+    assertEquals(bounds, a2.getBounds());
+    assertEquals(TestConstants.CONTENT_NAME, a2.getContentName());
+
+    verify(annotationSaver, only()).save(a2);
+
+  }
+
+  @Test
+  public void testWithProperties() throws IncompleteException {
+    Map<String, Object> properties2 = new HashMap<>();
+    properties2.put("key1", 17);
+    properties2.put("key2", false);
+
+    Annotation a3 = new SimpleAnnotation.Builder(TestConstants.CONTENT_NAME, annotationSaver)
+        .withType(TestConstants.ANNOTATION_TYPE)
+        .withBounds(bounds)
+        .withProperties(new SimpleMutableProperties(properties2))
+        .save();
     Map<String, Object> properties3 = a3.getProperties().getAll();
     assertEquals(2, properties3.size());
-    assertEquals(Integer.valueOf(17), properties3.get("key1"));
+    assertEquals(17, properties3.get("key1"));
     assertEquals(false, properties3.get("key2"));
+
+    verify(annotationSaver, only()).save(a3);
+
+  }
+
+  @Test
+  public void testFromExisting() throws IncompleteException {
+    Annotation a1 = new SimpleAnnotation.Builder(TestConstants.CONTENT_NAME, annotationSaver)
+        .withType(TestConstants.ANNOTATION_TYPE)
+        .withBounds(bounds)
+        .save();
+
+    clearInvocations(annotationSaver);
+
+    Annotation a2 = new SimpleAnnotation.Builder(TestConstants.CONTENT_NAME, annotationSaver)
+        .from(a1)
+        .withProperty("key1", 17)
+        .withProperty("key2", false)
+        .save();
+    assertNotNull(a2.getId());
+    assertEquals(a1.getId(), a2.getId());
+
+    assertEquals(TestConstants.ANNOTATION_TYPE, a2.getType());
+    assertEquals(bounds, a2.getBounds());
+    assertEquals(TestConstants.CONTENT_NAME, a2.getContentName());
+
+    Map<String, Object> properties2 = a2.getProperties().getAll();
+    assertEquals(2, properties2.size());
+    assertEquals(17, properties2.get("key1"));
+    assertEquals(false, properties2.get("key2"));
+
+    verify(annotationSaver, only()).save(a2);
+
+  }
+
+  @Test
+  public void testFromExistingWithNewId() throws IncompleteException {
+    Annotation a1 = new SimpleAnnotation.Builder(TestConstants.CONTENT_NAME, annotationSaver)
+        .withType(TestConstants.ANNOTATION_TYPE)
+        .withBounds(bounds)
+        .save();
+
+    clearInvocations(annotationSaver);
+
+    Annotation a2 = new SimpleAnnotation.Builder(TestConstants.CONTENT_NAME, annotationSaver)
+        .from(a1)
+        .newId()
+        .save();
+    assertNotNull(a2.getId());
+    assertNotEquals(a1.getId(), a2.getId());
+
+    verify(annotationSaver, only()).save(a2);
+
+  }
+
+  @Test
+  public void testFromExistingOverridden() throws IncompleteException {
+    Annotation a1 = new SimpleAnnotation.Builder(TestConstants.CONTENT_NAME, annotationSaver)
+        .withType(TestConstants.ANNOTATION_TYPE)
+        .withBounds(bounds)
+        .save();
+
+    clearInvocations(annotationSaver);
+
+    Bounds otherBounds = new TestBounds();
+    Annotation a2 = new SimpleAnnotation.Builder(TestConstants.CONTENT_NAME, annotationSaver)
+        .from(a1)
+        .withType("TEST2")
+        .withBounds(otherBounds)
+        .save();
+
+    assertEquals(a1.getId(), a2.getId());
+    assertEquals(a2.getType(), "TEST2");
+    assertEquals(a2.getBounds(), otherBounds);
+
+    // Check that a1 is unchecked
+    assertEquals(a1.getType(), TestConstants.ANNOTATION_TYPE);
+    assertEquals(a1.getBounds(), bounds);
+
+    verify(annotationSaver, only()).save(a2);
+
+
   }
 }
