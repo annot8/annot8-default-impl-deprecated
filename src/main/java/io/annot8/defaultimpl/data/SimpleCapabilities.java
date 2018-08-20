7ccc7bd761df4fabf3b686f87bc6be662a48148f
Chris Flatley <chris@committed.io>
2018-08-20 11:45:21 +0200

    Align with new Capabiliites interface

--- src/main/java/io/annot8/defaultimpl/data/SimpleCapabilities.java
+++ src/main/java/io/annot8/defaultimpl/data/SimpleCapabilities.java
@@ -1,7 +1,12 @@
 package io.annot8.defaultimpl.data;
 
 import io.annot8.core.bounds.Bounds;
+import io.annot8.core.capabilities.AnnotationCapability;
 import io.annot8.core.capabilities.Capabilities;
+import io.annot8.core.capabilities.Capabilities.Builder;
+import io.annot8.core.capabilities.ContentCapability;
+import io.annot8.core.capabilities.GroupCapability;
+import io.annot8.core.capabilities.ResourceCapability;
 import io.annot8.core.components.Resource;
 import io.annot8.core.data.Content;
 import java.util.Collections;
@@ -11,168 +16,167 @@ import java.util.stream.Stream;
 
 public class SimpleCapabilities implements Capabilities {
 
-  private final Set<String> requiredInputAnnotations;
-  private final Set<String> optionalInputAnnotations;
-  private final Set<String> outputAnnotations;
-  
-  private final Set<String> requiredInputGroups;
-  private final Set<String> optionalInputGroups;
-  private final Set<String> outputGroups;
-  
-  private final Set<Class<? extends Content<?>>> createdContent;
-  private final Set<Class<? extends Content<?>>> requiredContent;
+  private final Set<AnnotationCapability> processedAnnotations;
+  private final Set<AnnotationCapability> createdAnnotations;
+  private final Set<AnnotationCapability> deletedAnnotations;
 
-  private final Set<Class<? extends Resource>> requiredResources;
-  private final Set<Class<? extends Bounds>> outputBounds;
+  private final Set<GroupCapability> processedGroups;
+  private final Set<GroupCapability> createdGroups;
+  private final Set<GroupCapability> deletedGroups;
 
+  private final Set<ContentCapability> processedContent;
+  private final Set<ContentCapability> createdContent;
+  private final Set<ContentCapability> deletedContent;
 
-  private SimpleCapabilities(Set<String> requiredInputAnnotations,
-      Set<String> optionalInputAnnotations, Set<String> outputAnnotations,
-      Set<String> requiredInputGroups,
-      Set<String> optionalInputGroups, Set<String> outputGroups,
-      Set<Class<? extends Content<?>>> createdContent,
-      Set<Class<? extends Content<?>>> requiredContent,
-      Set<Class<? extends Resource>> requiredResources,
-      Set<Class<? extends Bounds>> outputBounds) {
-    this.requiredInputAnnotations = requiredInputAnnotations;
-    this.optionalInputAnnotations = optionalInputAnnotations;
-    this.outputAnnotations = outputAnnotations;
-    this.requiredInputGroups = requiredInputGroups;
-    this.optionalInputGroups = optionalInputGroups;
-    this.outputGroups = outputGroups;
+  private final Set<ResourceCapability> usedResources;
+
+  public SimpleCapabilities(Set<AnnotationCapability> processedAnnotations, Set<AnnotationCapability> createdAnnotations,
+      Set<AnnotationCapability> deletedAnnotations, Set<GroupCapability> processedGroups, Set<GroupCapability> createdGroups,
+      Set<GroupCapability> deletedGroups, Set<ContentCapability> processedContent, Set<ContentCapability> createdContent,
+      Set<ContentCapability> deletedContent, Set<ResourceCapability> usedResources) {
+
+    this.processedAnnotations = processedAnnotations;
+    this.createdAnnotations = createdAnnotations;
+    this.deletedAnnotations = deletedAnnotations;
+    this.processedGroups = processedGroups;
+    this.createdGroups = createdGroups;
+    this.deletedGroups = deletedGroups;
+    this.processedContent = processedContent;
     this.createdContent = createdContent;
-    this.requiredContent = requiredContent;
-    this.requiredResources = requiredResources;
-    this.outputBounds = outputBounds;
+    this.deletedContent = deletedContent;
+    this.usedResources = usedResources;
   }
 
   @Override
-  public Stream<String> getRequiredAnnotations() {
-    return requiredInputAnnotations.stream();
+  public Stream<AnnotationCapability> getProcessedAnnotations() {
+    return processedAnnotations.stream();
   }
 
   @Override
-  public Stream<String> getOptionalAnnotations() {
-    return optionalInputAnnotations.stream();
+  public Stream<AnnotationCapability> getCreatedAnnotations() {
+    return createdAnnotations.stream();
   }
 
   @Override
-  public Stream<String> getCreatedAnnotations() {
-    return outputAnnotations.stream();
-  }
-  
-  @Override
-  public Stream<String> getRequiredGroups() {
-    return requiredInputGroups.stream();
+  public Stream<AnnotationCapability> getDeletedAnnotations() {
+    return deletedAnnotations.stream();
   }
 
   @Override
-  public Stream<String> getOptionalGroups() {
-    return optionalInputGroups.stream();
+  public Stream<GroupCapability> getProcessedGroups() {
+    return processedGroups.stream();
   }
 
   @Override
-  public Stream<String> getCreatedGroups() {
-    return outputGroups.stream();
+  public Stream<GroupCapability> getCreatedGroups() {
+    return createdGroups.stream();
   }
 
   @Override
-  public Stream<Class<? extends Content<?>>> getRequiredContent() {
-    return requiredContent.stream();
+  public Stream<GroupCapability> getDeletedGroups() {
+    return deletedGroups.stream();
   }
 
   @Override
-  public Stream<Class<? extends Content<?>>> getCreatedContent() {
+  public Stream<ContentCapability> getCreatedContent() {
     return createdContent.stream();
   }
 
   @Override
-  public Stream<Class<? extends Resource>> getUsedResources() {
-    return requiredResources.stream();
+  public Stream<ContentCapability> getDeletedContent() {
+    return deletedContent.stream();
   }
 
   @Override
-  public Stream<Class<? extends Bounds>> getCreatedBounds() {
-    return outputBounds.stream();
+  public Stream<ContentCapability> getProcessedContent() {
+    return processedContent.stream();
   }
+
+  @Override
+  public Stream<ResourceCapability> getUsedResources() {
+    return usedResources.stream();
+  }
+
 
   public static class Builder implements Capabilities.Builder {
 
-    private final Set<String> requiredInputAnnotations = new HashSet<>();
-    private final Set<String> optionalInputAnnotations = new HashSet<>();
-    private final Set<String> outputAnnotations = new HashSet<>();
+    private final Set<AnnotationCapability> processedAnnotations = new HashSet<>();
+    private final Set<AnnotationCapability> createdAnnotations = new HashSet<>();
+    private final Set<AnnotationCapability> deletedAnnotations = new HashSet<>();
     
-    private final Set<String> requiredInputGroups = new HashSet<>();
-    private final Set<String> optionalInputGroups = new HashSet<>();
-    private final Set<String> outputGroups = new HashSet<>();
+    private final Set<GroupCapability> processedGroups = new HashSet<>();
+    private final Set<GroupCapability> createdGroups = new HashSet<>();
+    private final Set<GroupCapability> deletedGroups = new HashSet<>();
     
-    private final Set<Class<? extends Content<?>>> requiredContent = new HashSet<>();
-    private final Set<Class<? extends Content<?>>> createdContent = new HashSet<>();
-    private final Set<Class<? extends Resource>> requiredResources = new HashSet<>();
-    private final Set<Class<? extends Bounds>> outputBounds = new HashSet<>();
+    private final Set<ContentCapability> processedContent = new HashSet<>();
+    private final Set<ContentCapability> createdContent = new HashSet<>();
+    private final Set<ContentCapability> deletedContent = new HashSet<>();
 
-    public Builder requiresAnnotation(String type) {
-      requiredInputAnnotations.add(type);
+    private final Set<ResourceCapability> usedResources = new HashSet<>();
+
+    @Override
+    public Capabilities.Builder processesAnnotation(AnnotationCapability capability) {
       return this;
     }
 
-    public Builder optionalAnnotation(String type) {
-      optionalInputAnnotations.add(type);
+    @Override
+    public Capabilities.Builder createsAnnotation(AnnotationCapability capability) {
       return this;
     }
 
-    public Builder createsAnnotation(String type) {
-      outputAnnotations.add(type);
-      return this;
-    }
-    
-	public Builder requiresGroup(String type) {
-		requiredInputGroups.add(type);
-		return this;
-	}
-
-	public Builder optionalGroup(String type) {
-		optionalInputGroups.add(type);
-		return this;
-	}
-
-	public Builder createsGroup(String type) {
-		outputGroups.add(type);
-		return this;
-	}
-
-    public Builder requiresContent(Class<? extends Content<?>> clazz) {
-      requiredContent.add(clazz);
+    @Override
+    public Capabilities.Builder deletesAnnotation(AnnotationCapability capability) {
       return this;
     }
 
-    public Builder createsContent(Class<? extends Content<?>> clazz) {
-      createdContent.add(clazz);
+    @Override
+    public Capabilities.Builder processesGroup(GroupCapability capability) {
       return this;
     }
 
-    public Builder usesResource(Class<? extends Resource> clazz) {
-      requiredResources.add(clazz);
+    @Override
+    public Capabilities.Builder createsGroup(GroupCapability capability) {
       return this;
     }
 
-    public Builder createsBounds(Class<? extends Bounds> clazz) {
-      outputBounds.add(clazz);
+    @Override
+    public Capabilities.Builder deletesGroup(GroupCapability capability) {
       return this;
     }
 
+    @Override
+    public Capabilities.Builder processesContent(ContentCapability capability) {
+      return this;
+    }
+
+    @Override
+    public Capabilities.Builder createsContent(ContentCapability capability) {
+      return this;
+    }
+
+    @Override
+    public Capabilities.Builder deletesContent(ContentCapability capability) {
+      return this;
+    }
+
+    @Override
+    public Capabilities.Builder usesResource(ResourceCapability capability) {
+      return this;
+    }
+
+    @Override
     public Capabilities save() {
       return new SimpleCapabilities(
-          Collections.unmodifiableSet(requiredInputAnnotations),
-          Collections.unmodifiableSet(optionalInputAnnotations),
-          Collections.unmodifiableSet(outputAnnotations),
-          Collections.unmodifiableSet(requiredInputGroups),
-          Collections.unmodifiableSet(optionalInputGroups),
-          Collections.unmodifiableSet(outputGroups),
+          Collections.unmodifiableSet(processedAnnotations),
+          Collections.unmodifiableSet(createdAnnotations),
+          Collections.unmodifiableSet(deletedAnnotations),
+          Collections.unmodifiableSet(processedGroups),
+          Collections.unmodifiableSet(createdGroups),
+          Collections.unmodifiableSet(deletedGroups),
+          Collections.unmodifiableSet(processedContent),
           Collections.unmodifiableSet(createdContent),
-          Collections.unmodifiableSet(requiredContent),
-          Collections.unmodifiableSet(requiredResources),
-          Collections.unmodifiableSet(outputBounds));
+          Collections.unmodifiableSet(deletedContent),
+          Collections.unmodifiableSet(usedResources));
     }
 
   }
