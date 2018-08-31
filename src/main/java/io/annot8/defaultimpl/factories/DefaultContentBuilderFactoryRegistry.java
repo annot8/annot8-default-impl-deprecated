e5feafc3a4df9428d6ae9231caa4e2a2f95e9732
Chris Flatley <chris@committed.io>
2018-08-31 12:50:05 +0100

    Default content builder regsitry factory now includes available content types

--- src/main/java/io/annot8/defaultimpl/factories/DefaultContentBuilderFactoryRegistry.java
+++ src/main/java/io/annot8/defaultimpl/factories/DefaultContentBuilderFactoryRegistry.java
@@ -4,26 +4,18 @@ package io.annot8.defaultimpl.factories;
 import io.annot8.common.data.content.FileContent;
 import io.annot8.common.data.content.InputStreamContent;
 import io.annot8.common.data.content.Text;
-import io.annot8.common.implementations.factories.ContentBuilderFactory;
-import io.annot8.common.implementations.registries.ContentBuilderFactoryRegistry;
-import io.annot8.core.data.Content;
+import io.annot8.common.implementations.registries.SimpleContentBuilderFactoryRegistry;
 import io.annot8.defaultimpl.content.SimpleFile;
 import io.annot8.defaultimpl.content.SimpleInputStream;
 import io.annot8.defaultimpl.content.SimpleText;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
-public class SimpleContentBuilderFactoryRegistry implements ContentBuilderFactoryRegistry {
+public class DefaultContentBuilderFactoryRegistry extends SimpleContentBuilderFactoryRegistry {
 
-  private final Map<Class<? extends Content<?>>, ContentBuilderFactory<?, ?>> builders = new HashMap<>();
-
-
-  public SimpleContentBuilderFactoryRegistry() {
+  public DefaultContentBuilderFactoryRegistry() {
     this(true);
   }
 
-  public SimpleContentBuilderFactoryRegistry(boolean includeDefaultContentBuilders) {
+  public DefaultContentBuilderFactoryRegistry(boolean includeDefaultContentBuilders) {
 
     if(includeDefaultContentBuilders) {
       register(Text.class, new SimpleText.BuilderFactory());
@@ -31,21 +23,6 @@ public class SimpleContentBuilderFactoryRegistry implements ContentBuilderFactor
       register(InputStreamContent.class, new SimpleInputStream.BuilderFactory());
 
     }
-  }
-
-  public <D, C extends Content<D>, I extends C> void register(Class<C> contentClass,
-      ContentBuilderFactory<D, I> contentBuilderFactory) {
-    builders.put(contentClass, contentBuilderFactory);
-  }
-
-  @Override
-  public <D, C extends Content<D>> Optional<ContentBuilderFactory<D, C>> get(
-      Class<C> contentClass) {
-    // This cast to C is correct, due to the way that the content is registered, we know that it tallies.
-    // TODO: Slightly unsure about cast to D, I think it's correct in our case
-    ContentBuilderFactory<D, C> contentBuilderFactory = (ContentBuilderFactory<D, C>) builders
-        .get(contentClass);
-    return Optional.ofNullable(contentBuilderFactory);
   }
 
 }
