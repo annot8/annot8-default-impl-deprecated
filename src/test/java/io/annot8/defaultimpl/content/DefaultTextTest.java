9fdb3c82e5ca0725b2ba2651c0d35b3001dab2e0
Steven Taylor <steven@committed.io>
2018-08-31 15:10:31 +0100

    Adds tests for DefaultItem

--- src/test/java/io/annot8/defaultimpl/content/DefaultTextTest.java
+++ src/test/java/io/annot8/defaultimpl/content/DefaultTextTest.java
@@ -2,18 +2,26 @@ package io.annot8.defaultimpl.content;
 
 import io.annot8.common.implementations.stores.NoOpSaveCallback;
 import io.annot8.testing.testimpl.TestConstants;
+import io.annot8.testing.testimpl.TestItem;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class DefaultTextTest {
-  
-      @Test
-      public void testBuilder(){
-          DefaultText.Builder builder = new DefaultText.Builder(new NoOpSaveCallback<>());
-          DefaultText defaultText = builder.create(TestConstants.CONTENT_ID, TestConstants.CONTENT_NAME, null, () -> "test");
-          Assertions.assertEquals(TestConstants.CONTENT_ID, defaultText.getId());
-          Assertions.assertEquals(TestConstants.CONTENT_NAME, defaultText.getName());
-          Assertions.assertEquals("test", defaultText.getData());
-      }
+
+    public void testBuilderFactory(){
+        DefaultText.BuilderFactory factory = new DefaultText.BuilderFactory();
+        assertNotNull(factory.create(new TestItem(), new NoOpSaveCallback<>()));
+    }
+
+    @Test
+    public void testBuilder(){
+        DefaultText.Builder builder = new DefaultText.Builder(new NoOpSaveCallback<>());
+        DefaultText defaultText = builder.create(TestConstants.CONTENT_ID, TestConstants.CONTENT_NAME, null, () -> "test");
+        Assertions.assertEquals(TestConstants.CONTENT_ID, defaultText.getId());
+        Assertions.assertEquals(TestConstants.CONTENT_NAME, defaultText.getName());
+        Assertions.assertEquals("test", defaultText.getData());
+    }
   
   }
