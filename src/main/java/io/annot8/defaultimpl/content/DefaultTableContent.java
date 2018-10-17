709334b11a96a2b8ecdb6b5604a83621cd530c03
Steven Taylor <steven@committed.io>
2018-10-17 09:35:12 +0100

    Apply spotless formatting

--- src/main/java/io/annot8/defaultimpl/content/DefaultTableContent.java
+++ src/main/java/io/annot8/defaultimpl/content/DefaultTableContent.java
@@ -1,4 +1,7 @@
+/* Annot8 (annot8.io) - Licensed under Apache-2.0. */
 package io.annot8.defaultimpl.content;
+
+import java.util.function.Supplier;
 
 import io.annot8.common.data.content.Table;
 import io.annot8.common.data.content.TableContent;
@@ -9,26 +12,31 @@ import io.annot8.core.data.BaseItem;
 import io.annot8.core.data.Content;
 import io.annot8.core.properties.ImmutableProperties;
 import io.annot8.defaultimpl.stores.DefaultAnnotationStore;
-import java.util.function.Supplier;
 
 public class DefaultTableContent extends AbstractContent<Table> implements TableContent {
 
-  private DefaultTableContent(String id, String name, ImmutableProperties properties,
-      Supplier<Table> dataSupplier){
-    super(Table.class, TableContent.class, new DefaultAnnotationStore(id), id, name,
-        properties, dataSupplier);
+  private DefaultTableContent(
+      String id, String name, ImmutableProperties properties, Supplier<Table> dataSupplier) {
+    super(
+        Table.class,
+        TableContent.class,
+        new DefaultAnnotationStore(id),
+        id,
+        name,
+        properties,
+        dataSupplier);
   }
 
   public static class Builder extends AbstractContentBuilder<Table, TableContent> {
 
     @Override
-    protected TableContent create(String id, String name, ImmutableProperties properties,
-        Supplier<Table> data) {
+    protected TableContent create(
+        String id, String name, ImmutableProperties properties, Supplier<Table> data) {
       return new DefaultTableContent(id, name, properties, data);
     }
   }
 
-  public static class BuilderFactory extends AbstractContentBuilderFactory<Table, TableContent>{
+  public static class BuilderFactory extends AbstractContentBuilderFactory<Table, TableContent> {
 
     public BuilderFactory() {
       super(Table.class, TableContent.class);
@@ -39,6 +47,4 @@ public class DefaultTableContent extends AbstractContent<Table> implements Table
       return new Builder();
     }
   }
-
-
 }
