49fe8756845a57df9561707e336deec1fbdcad80
Chris Flatley <chris@committed.io>
2019-03-08 11:14:20 +0000

    Add default content builders into registry

--- src/main/java/io/annot8/defaultimpl/factories/DefaultContentBuilderFactoryRegistry.java
+++ src/main/java/io/annot8/defaultimpl/factories/DefaultContentBuilderFactoryRegistry.java
@@ -3,11 +3,15 @@ package io.annot8.defaultimpl.factories;
 
 import io.annot8.common.data.content.FileContent;
 import io.annot8.common.data.content.InputStreamContent;
+import io.annot8.common.data.content.TableContent;
 import io.annot8.common.data.content.Text;
+import io.annot8.common.data.content.UriContent;
 import io.annot8.common.implementations.registries.SimpleContentBuilderFactoryRegistry;
 import io.annot8.defaultimpl.content.DefaultFile;
 import io.annot8.defaultimpl.content.DefaultInputStream;
+import io.annot8.defaultimpl.content.DefaultTableContent;
 import io.annot8.defaultimpl.content.DefaultText;
+import io.annot8.defaultimpl.content.DefaultUri;
 
 public class DefaultContentBuilderFactoryRegistry extends SimpleContentBuilderFactoryRegistry {
 
@@ -21,6 +25,8 @@ public class DefaultContentBuilderFactoryRegistry extends SimpleContentBuilderFa
       register(Text.class, new DefaultText.BuilderFactory());
       register(FileContent.class, new DefaultFile.BuilderFactory());
       register(InputStreamContent.class, new DefaultInputStream.BuilderFactory());
+      register(TableContent.class, new DefaultTableContent.BuilderFactory());
+      register(UriContent.class, new DefaultUri.BuilderFactory());
     }
   }
 }
